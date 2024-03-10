f2a5d2aa1978ff9dcfb798a045e99e53e65f3ebf
Alvaro Rumpel <alvaroRumpel@gmail.com>
2024-03-10 20:17:42 -0300

    update terraform

--- vpc.tf
+++ vpc.tf
@@ -13,16 +13,42 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+data "aws_subnet" "existing_subnet_a" {
+  filter {
+    name   = "tag:Name"
+    values = ["subnet-a-rds-vpc"]
+  }
+}
+
+data "aws_subnet" "existing_subnet_b" {
+  filter {
+    name   = "tag:Name"
+    values = ["subnet-b-rds-vpc"]
+  }
+}
+
+
 resource "aws_subnet" "subnet_a" {
+  count      = length(data.aws_subnet.existing_subnet_a.ids) > 0 ? 0 : 1
   vpc_id     = data.aws_vpc.existing_vpc.id != null ? data.aws_vpc.existing_vpc.id : aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-2a"
+
+  tags = {
+    Name: "subnet-a-rds-vpc"
+  }
 }
 
 resource "aws_subnet" "subnet_b" {
+  count      = length(data.aws_subnet.existing_subnet_b.ids) > 0 ? 0 : 1
   vpc_id     = data.aws_vpc.existing_vpc.id != null ? data.aws_vpc.existing_vpc.id : aws_vpc.vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-2b"
+
+  
+  tags = {
+    Name: "subnet-b-rds-vpc"
+  }
 }
 
 resource "aws_security_group" "rds_sg" {
