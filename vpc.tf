08d9e971a25e81e29c1de71d8baa1cacdc79abfa
Alvaro Rumpel <alvaroRumpel@gmail.com>
2024-03-10 20:37:38 -0300

    revert changes

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,3 @@
-data "aws_vpc" "existing_vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["rds-vpc"]
-  }
-}
-
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -14,7 +7,7 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "subnet_a" {
-  vpc_id     = data.aws_vpc.existing_vpc.id != null ? data.aws_vpc.existing_vpc.id : aws_vpc.vpc.id
+  vpc_id     = data.aws_vpc.existing_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-2a"
 
@@ -24,7 +17,7 @@ resource "aws_subnet" "subnet_a" {
 }
 
 resource "aws_subnet" "subnet_b" {
-  vpc_id     = data.aws_vpc.existing_vpc.id != null ? data.aws_vpc.existing_vpc.id : aws_vpc.vpc.id
+  vpc_id     = data.aws_vpc.existing_vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-2b"
 
@@ -36,7 +29,7 @@ resource "aws_subnet" "subnet_b" {
 resource "aws_security_group" "rds_sg" {
   name_prefix = "rds-"
 
-  vpc_id = data.aws_vpc.existing_vpc.id != null ? data.aws_vpc.existing_vpc.id : aws_vpc.vpc.id
+  vpc_id = data.aws_vpc.existing_vpc.id
 
   ingress {
     from_port   = 3306
