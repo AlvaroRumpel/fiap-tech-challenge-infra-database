4596516fad09a2a4576153b3d4304be7b4d869bc
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2024-03-11 14:00:00 -0300

    adjusts

--- main.tf
+++ main.tf
@@ -23,10 +23,10 @@ module "vpc" {
   version = "5.0.0"
 
   name                 = "rds-vpc"
-  cidr                 = "172.16.0.0/16"
+  cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
