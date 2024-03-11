2d57e0ed3881854c137a4a27dedabcc77d3a20ef
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2024-03-11 14:18:04 -0300

    adjusts

--- main.tf
+++ main.tf
@@ -36,13 +36,13 @@ resource "aws_security_group" "rds_sg" {
 }
 
 resource "aws_subnet" "rds_subnet" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-2a"
 }
 
 resource "aws_subnet" "rds_subnet1" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-2b"
 }
